59d349003ff51a8037f541993ff5d40ec5c5917b
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-25 17:52:15 -0500

    Encriptando contraseña en el registro

--- blog/users/api/serializers.py
+++ blog/users/api/serializers.py
@@ -10,3 +10,10 @@ class UserRegisterSerializer(serializers.ModelSerializer):
             'username',
             'password',
         ]
+    def create(self, validated_data):
+        password = validated_data.pop('password',None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
