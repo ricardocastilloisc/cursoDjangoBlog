0b48f800b269a1a6f23623ada189dc0cb79addd3
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-25 16:55:17 -0500

    documentacion de api

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',
     'users'
 ]
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = './static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
