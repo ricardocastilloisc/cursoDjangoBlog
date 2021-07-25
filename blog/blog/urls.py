bcbac0cbde1b4a6b55ae5fc37e85c2399b74de7c
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-25 17:35:34 -0500

    logica de registro usuario

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -22,9 +22,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Documetacion",
+      title="Documentacion API BLOG",
       default_version='v1',
-      description="Documentacion de Blog",
+      description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
@@ -36,6 +36,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.api.router')),
-    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- blog/users/api/views.py
+++ blog/users/api/views.py
@@ -2,8 +2,12 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from users.models import User
+from users.api.serializers import UserRegisterSerializer
 
 class RegisterView(APIView):
     def post(self, request):
-        print('Registrando usuario')
-        return Response(status=status.HTTP_200_OK)
+        serializer =  UserRegisterSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST )
