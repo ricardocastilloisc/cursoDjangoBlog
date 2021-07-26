bb850a28eb8a3307da46f2097b0cb6316f1b1800
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-26 09:55:25 -0500

    Solo el usuario puede eliminar o actualizar su comen

--- blog/coments/api/permissions.py
+++ blog/coments/api/permissions.py
@@ -1,8 +1,16 @@
 from rest_framework.permissions import BasePermission
+from coments.models import Comment
 
-class IsAdminOrReadOnly(BasePermission):
+class IsOwnerOrReadAndCreateOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method == 'GET':
+        if request.method == 'GET' or request.method == 'POST':
             return True
         else:
-            return request.user.is_staff
+            id_coment = view.kwargs['pk']
+            coment = Comment.objects.get(pk=id_coment)
+            id_user = request.user.pk
+            id_user_coment = coment.user_id
+
+            if id_user_coment == id_user:
+                return True
+            return False
--- blog/coments/api/views.py
+++ blog/coments/api/views.py
@@ -4,8 +4,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 from coments.models import Comment
 from coments.api.serializers import CommentSerializer
+from coments.api.permissions import IsOwnerOrReadAndCreateOnly
 
 class CommentApiViewSet(ModelViewSet):
+    permission_classes = [IsOwnerOrReadAndCreateOnly]
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
     filter_backends = [OrderingFilter, DjangoFilterBackend]
