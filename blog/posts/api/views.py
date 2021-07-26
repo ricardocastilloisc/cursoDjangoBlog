836864c6d24889cfda0e051cc98006d6dc2837a6
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-26 08:40:41 -0500

    filtrar por una propiedad de un expliegue se hace __

--- blog/posts/api/views.py
+++ blog/posts/api/views.py
@@ -2,8 +2,13 @@ from posts.api.permissions import IsAdminOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from posts.models import Post
 from posts.api.serializers import PostSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class PostApiViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(published=True)
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category','category__slug']
+    #filterset_fields = ['category']
