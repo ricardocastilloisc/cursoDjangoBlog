6b1abdd5bd94a8419cfcf46ba9f3eefa9f4fa12e
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-26 09:28:30 -0500

    filtrar por id de post

--- blog/coments/api/views.py
+++ blog/coments/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from coments.models import Comment
 from coments.api.serializers import CommentSerializer
@@ -7,6 +8,7 @@ from coments.api.serializers import CommentSerializer
 class CommentApiViewSet(ModelViewSet):
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
-    filter_backends = [OrderingFilter]
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    filterset_fields = ['post']
     ordering = ['-created_at']
 
