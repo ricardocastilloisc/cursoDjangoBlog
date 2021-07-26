5e3fee485fcf89d256ac8e0b2c4eba10639aa45f
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-25 22:39:35 -0500

    permisos de crud de categorias

--- blog/categories/api/views.py
+++ blog/categories/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from categories.models import Category
 from categories.api.serializers import CategorySerializer
+from categories.api.permission import IsAdminOrReadOnly
 
 class CategoryApiViewSet(ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
