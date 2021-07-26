d4c6f6cb5434ac1c17d2d241365e06dc34c3fce1
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-25 21:58:22 -0500

    Creando el CRUD de category

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from  categories.api.router import router_categories
+
 schema_view = get_schema_view(
    openapi.Info(
       title="Documentacion API BLOG",
@@ -36,6 +38,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.api.router')),
+    path('api/', include(router_categories.urls)),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
